a95e6feb2fb9cf8b4318c47f095ac2ff4d9ba74b
Md Mohosin <mohosinali075@gmail.com>
2025-06-30 06:16:52 +0600

    Modified notification healper

--- src/helpers/notificationsHelper.ts
+++ src/helpers/notificationsHelper.ts
@@ -1,17 +1,39 @@
+// This file created by Md Nadir V.
+// import { INotification } from './../app/modules/notification/notification.interface';
+// import { ClientSession } from "mongoose";
+
+
+// export const sendNotifications = async (payload:INotification, session?: ClientSession):Promise<INotification> =>{
+
+//     const result = (await Notification.create([payload], {session}))[0];
+
+//     //@ts-ignore
+//     const socketIo = global.io;
+
+//     if (socketIo && payload.receiver) {
+//         socketIo.emit(`notification::${payload.receiver}`, result);
+//     }
+
+//     return result;
+// }
+
+
+// Added by: Md Mohosin
 import { INotification } from './../app/modules/notification/notification.interface';
+import { Notification } from './../app/modules/notification/notification.model';
 import { ClientSession } from "mongoose";
 
+export const sendNotifications = async (
+  payload: INotification,
+  session?: ClientSession
+): Promise<INotification> => {
+  const [result] = await Notification.create([payload], { session });
+//@ts-ignore
+  const socketIo = global.io;
 
-export const sendNotifications = async (payload:INotification, session?: ClientSession):Promise<INotification> =>{
+  if (socketIo && payload.receiver) {
+    socketIo.emit(`notification::${payload.receiver}`, result);
+  }
 
-    const result = (await Notification.create([payload], {session}))[0];
-
-    //@ts-ignore
-    const socketIo = global.io;
-
-    if (socketIo && payload.receiver) {
-        socketIo.emit(`notification::${payload.receiver}`, result);
-    }
-
-    return result;
-}
+  return result;
+};
